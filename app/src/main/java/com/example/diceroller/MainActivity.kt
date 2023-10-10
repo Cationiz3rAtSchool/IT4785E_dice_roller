f40bb63dcfca7cc0caede3249c2642e6193aa44e
Martin Cation <martincation@protonmail.com>
2023-10-10 22:43:36 +0700

    app: remove default components & add toast on button click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,46 +1,17 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.diceroller.ui.theme.DiceRollerTheme
-
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContent {
-			DiceRollerTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
-				) {
-					Greeting("Android")
-				}
-			}
-		}
-	}
-}
+		setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	DiceRollerTheme {
-		Greeting("Android")
+		val rollButton: Button = findViewById(R.id.button)
+		rollButton.setOnClickListener({
+			Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+		});
 	}
 }
