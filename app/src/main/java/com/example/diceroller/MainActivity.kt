a9dc5684b2861c2a82e9eb42f512c16b4a441798
Martin Cation <martincation@protonmail.com>
2023-10-10 22:48:25 +0700

    app: create Dice obj & use that to set random number text

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,21 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+
+class Dice(val nSides: Int) {
+	fun roll(): Int {
+		return (1..nSides).random()
+	}
+}
 class MainActivity : ComponentActivity() {
+	private fun rollDice() {
+		val result = Dice(6).roll()
+		val resultTextView: TextView = findViewById(R.id.textView)
+		resultTextView.text = result.toString()
+	}
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
@@ -12,6 +24,7 @@ class MainActivity : ComponentActivity() {
 		val rollButton: Button = findViewById(R.id.button)
 		rollButton.setOnClickListener({
 			Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+			rollDice()
 		});
 	}
 }
